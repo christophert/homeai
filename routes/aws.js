1bab557373b38ef74e1246893a7b622611b54091
Christopher Tran <me@christran.in>
2017-11-05 10:23:58 -0500

    move requests out of route and into ajax and add more graphs to the front page

--- routes/aws.js
+++ routes/aws.js
@@ -6,17 +6,12 @@ AWS.config.apiVersions = { dynamodb: 'latest' }
 AWS.config.update({region: 'us-east-1'});
 
 var db = new AWS.DynamoDB.DocumentClient();
-var params = {
-    TableName: 'heartRate',
-};
 
-//router.get('/table-list', function(req, res, next) {
-    //db.listTables(function(err, data) {
-        //console.log(data.TableNames);
-    //});
-    //res.send('good');
-//});
+
 router.get('/hr-data', function(req, res, next) {
+    var params = {
+        TableName: 'heartRate',
+    };
     var DataArray = [];
     db.scan(params, function(err, data) {
         if(err) {
@@ -30,4 +25,54 @@ router.get('/hr-data', function(req, res, next) {
     });
 });
 
+router.get('/oc-data', function(req, res, next) {
+    var params = {
+        TableName: 'occupancy',
+    };
+    var DataArray = [];
+    db.scan(params, function(err, data) {
+        if(err) {
+            console.log("Error", err);
+        } else {
+            data.Items.forEach(function(element, index, array) {
+                DataArray.push(element);
+            });
+            res.send(DataArray);
+        }
+    });
+});
+
+router.get('/st-data', function(req, res, next) {
+    var params = {
+        TableName: 'stats',
+    };
+    var DataArray = [];
+    db.scan(params, function(err, data) {
+        if(err) {
+            console.log("Error", err);
+        } else {
+            data.Items.forEach(function(element, index, array) {
+                DataArray.push(element);
+            });
+            res.send(DataArray);
+        }
+    });
+});
+
+router.get('/pr-data', function(req, res, next) {
+    var params = {
+        TableName: 'predictions',
+    };
+    var DataArray = [];
+    db.scan(params, function(err, data) {
+        if(err) {
+            console.log("Error", err);
+        } else {
+            data.Items.forEach(function(element, index, array) {
+                DataArray.push(element);
+            });
+            res.send(DataArray);
+        }
+    });
+});
 module.exports = router;
